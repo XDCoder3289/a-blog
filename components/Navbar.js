f43a22a2ef0af3d4c683f9a308af959bd0e44afb
Salman <salman@MacBook-Air.local>
2023-04-10 14:58:53 +0500

    Changing Navbar to use hooks

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,36 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
+
+  const isActiveLink = (href) => {
+    return router.pathname === href;
+  };
+
+  const activeClass = "text-cyan-600";
+  const inActiveClass = "";
+
   return (
     <div>
       <nav className="">
-        <ul className="flex justify-center space-x-8 mt-11 text-xl text-slate-300">
+        <ul className="flex justify-center space-x-8 mt-11 text-xl text-slate-300 text-">
           <Link href="/">
-            <li>Home</li>
+            <li className={isActiveLink("/") ? activeClass : inActiveClass}>
+              Home
+            </li>
           </Link>
           <Link href="/about">
-            <li>About</li>
+            <li
+              className={isActiveLink("/about") ? activeClass : inActiveClass}
+            >
+              About
+            </li>
           </Link>
           <Link href="/blog">
-            <li>Blogs</li>
+            <li className={isActiveLink("/blog") ? activeClass : inActiveClass}>
+              Blogs
+            </li>
           </Link>
         </ul>
       </nav>
